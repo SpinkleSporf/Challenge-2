cbff1ec19a2e5b15c98e1a86199a0969a2b65385
Matt Brennan <mattbrennan1123@gmail.com>
2023-04-17 14:02:10 -0400

    finished bonus

--- Play Fetch Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Play Fetch Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public int cooldown = 1;
+    private float timeStamp;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timeStamp <= Time.time)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeStamp = Time.time + cooldown;
         }
     }
 }
--- Play Fetch Challenge/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Play Fetch Challenge/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,11 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
@@ -27,6 +26,10 @@ public class SpawnManagerX : MonoBehaviour
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        //creates the random spawn interval
+        int spawnInterval = Random.Range(3, 6);
+
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
 }
